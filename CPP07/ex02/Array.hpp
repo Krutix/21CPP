0f917a95154bf50254984b27f7262144570a089a
Krutix <pigeon.krutix@gmail.com>
2021-09-08 01:30:19 +0300

    [07] strong exception handle

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,17 +6,32 @@
 #include <string>
 
 template<typename T>
+struct autofree_ptr
+{
+    T* m_ptr;
+
+    autofree_ptr(T* ptr = NULL) : m_ptr(ptr) {}
+    ~autofree_ptr() { delete[] m_ptr; }
+    T* release() { T* tmp = m_ptr; m_ptr = NULL; return tmp; }
+private:
+    autofree_ptr(autofree_ptr const&);
+    autofree_ptr& operator = (autofree_ptr const&);
+};
+
+template<typename T>
 class Array
 {
 public:
     Array(size_t size = 0)
-        : m_size(size), m_array(new T[size]) { }
+        : m_size(size), m_array(new T[size]) { } // TODO learn abount opertor new behavior when default constructor throw exception
 
     Array(Array<T> const& right)
-        : m_size(right.m_size), m_array(new T[right.m_size])
+        : m_size(right.m_size)
     {
+        autofree_ptr<T> new_arr(new T[m_size]);
         for (size_t i = 0; i < m_size; i++)
-            m_array[i] = right.m_array[i];
+            new_arr.m_ptr[i] = right.m_array[i];
+        m_array = new_arr.release();
     }
 
     ~Array()
@@ -28,8 +43,14 @@ public:
     {
         if (&right == this)
             return *this;
-        this->~Array();
-        new (this)Array<T>(right);
+
+        autofree_ptr<T> new_array(new T[right.m_size]);
+        for (size_t i = 0; i < right.m_size; i++)
+            new_array.m_ptr[i] = right.m_array[i];
+
+        m_size = right.m_size;
+        delete[] m_array;
+        m_array = new_array.release();
         return *this;
     }
 
@@ -45,6 +66,6 @@ public:
         return const_cast<T&>(const_cast<Array<T> const&>(*this)[pos]);
     }
 private:
-    size_t m_size;
-    T*     m_array;
+    size_t  m_size;
+    T*      m_array;
 };
